2bf9d51d0fb3fa7056e6caba1563adf303f612b8
Zi-nat <zinat_ahmadi@yahoo.com>
2021-01-15 12:29:44 +0100

    adding a map to see the ingredients element

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,14 +1,26 @@
 import React from 'react';
 
 import classes from './Burger.css';
-import BurgerIngredients from './Ingeredients';
+import BurgerIngredients from './Ingredients/Ingredients';
 
 const burger = (props) =>{   
-    return (
+      let transformedIngredient = Object.keys(props.ingredient)
+        .map (igKey => {
+            return [...Array( props.ingredient[igKey] )].map( ( _, i ) => {
+                return <BurgerIngredients key={igKey + i} type={igKey} />;
+            });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+
+        if (transformedIngredient.length === 0) {
+            transformedIngredient = <p>Please start adding ingredients!</p>;}
+  return (
             
             <div className={classes.Burger}>
               <BurgerIngredients type= "bread-top"/>
-              <BurgerIngredients type= "meat"></BurgerIngredients>
+              {transformedIngredient}
               <BurgerIngredients type= "bread-bottom"></BurgerIngredients>
             </div>
             
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
+//import BuildControl from '../../components/Burger/BuildControl/BuildControl';
 
 class BurgerBuilder extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {...}
+    // }
+    state = {
+      ingredient: {
+          salad: 0,
+          cheese:0,
+          meat: 0
+      }
+    }
   render() {
     return (
       <Auxiliary>
-        <Burger/>
-        <div>Build Controls</div>
+        <Burger ingredient={this.state.ingredient} />
+       
       </Auxiliary>
     );
   }
