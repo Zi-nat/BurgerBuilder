74e5deca3e8f23402256059fe41cdef0ab013982
Zi-nat <zinat_ahmadi@yahoo.com>
2021-01-15 16:37:11 +0100

    config the disable function of Less-buttom

--- src/components/Burger/BuildControl/ControlButtom/ControlButtom.js
+++ src/components/Burger/BuildControl/ControlButtom/ControlButtom.js
@@ -4,8 +4,15 @@ import classes from './ControlButtom.css';
 const controlButtom = (props) => (
     <div className= {classes.BuildControl}>
         <div className={classes.Label}>{props.label}</div>
-        <button className={classes.Less}>Less</button>
-        <button className={classes.More} onClick ={props.added}>More</button>
+        <button 
+          className={classes.Less} 
+          onClick = {props.removed}
+          disabled={props.disabled}>Less
+        </button>
+        <button 
+          className={classes.More} 
+          onClick ={props.added}>More
+        </button>
     </div>
 );   
 export default controlButtom;
