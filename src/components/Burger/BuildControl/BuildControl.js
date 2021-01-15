c8767c782b005ac27f22d604edfb3d6420d41174
Zi-nat <zinat_ahmadi@yahoo.com>
2021-01-15 12:54:24 +0100

    adding a UI Component (less & more buttom) to add dynamicly ingredients

---
+++ src/components/Burger/BuildControl/BuildControl.js
@@ -0,0 +1,20 @@
+
+import React from 'react';//Here user decided witch ingredients the Burger should have
+import classes from './BuildControl.css';
+import ControlButtom from './ControlButtom/ControlButtom';
+
+const controls = [
+    {label: 'Salad', type: 'salad'},
+    {label: 'Cheese', type: 'cheese'},
+    {label: 'Meat', type: 'meat'},
+];
+
+const buildControl = (props) => (
+   <div className={classes.BuildControl}>
+       {controls.map(ctrl => (
+       <ControlButtom key={ctrl.label} label={ctrl.label}/>
+       ))}
+   </div>
+);
+
+export default buildControl;
